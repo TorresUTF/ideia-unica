e3f98c007d6255fafe851d43a75707efc312d54a
Gabriel Torres Andreassa <70169977+TorresUTF@users.noreply.github.com>
2022-02-13 16:00:53 -0300

    primeira pag

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
-import { useState} from 'react';
-
 function Home() {
     return (
-        <div>
-            <h2>  Home  </h2>
-            <Contador/>
-        </div>
+        < !DOCTYPE html >
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <link rel="stylesheet" href="style.css">
+                                <title>Document</title>
+                            </head>
+                            <body>
+                                <div class="principal">
+                                    <header>
+                                        <p>Header</p>
+                                    </header>
+                                    <section>
+                                        <p>conteudo</p>
+                                    </section>
+                                    <aside>
+                                        <p>sidebar</p>
+                                    </aside>
+                                    <footer>
+                                        <div class="parte1"><p>parte 1</p></div>
+                                        <div class="parte2"><p>parte 2</p></div>
+                                        <div class="parte3"><p>parte 3</p></div>
+                                    </footer>
+                                </div>
+                            </body>
+                        </html>
     )
 }
 
 export default Home
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>
-                Adicionar
-            </button>
-        </div>
-    )
-}
